c5aaddd385cba17de1d034ddb7f6eb4b86c80f66
Philip Tenteromano <tenteromano.p@gmail.com>
2022-03-16 18:14:58 -0400

    photos path production bug

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,16 +1,17 @@
 import { LoaderFunction, useLoaderData } from "remix";
 import styles from "~/styles/carousel.css";
 import fs from "fs";
+import path from "path";
 
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
 }
 
 export const loader: LoaderFunction = () => {
-  const testFolder = "public/photos/";
-
+  const photosPath = path.join(__dirname, "../", "public/photos/");
+  console.log({ photosPath });
   const fileNames: Array<string> = [];
-  fs.readdirSync(testFolder).forEach((file) => {
+  fs.readdirSync(photosPath).forEach((file) => {
     fileNames.push(file);
   });
 
@@ -28,7 +29,6 @@ export default function Index() {
               src={`/photos/${filename}`}
               alt={`Madelyn and Phil ${filename}`}
               className="carousel-image"
-
             />
           </div>
         );
