5745ab74e5134a5293606cec5b8d0fc4d65bb74a
Philip Tenteromano <tenteromano.p@gmail.com>
2022-03-16 18:23:41 -0400

    just use static

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,25 +1,11 @@
-import { LoaderFunction, useLoaderData } from "remix";
 import styles from "~/styles/carousel.css";
-import fs from "fs";
-import path from "path";
+import fileNames from "~/data/photos";
 
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
 }
 
-export const loader: LoaderFunction = () => {
-  const photosPath = path.join(__dirname, "../", "public/photos/");
-  console.log({ photosPath });
-  const fileNames: Array<string> = [];
-  fs.readdirSync(photosPath).forEach((file) => {
-    fileNames.push(file);
-  });
-
-  return fileNames;
-};
-
 export default function Index() {
-  const fileNames = useLoaderData<Array<string>>();
   return (
     <div className="horizontal-snap">
       {fileNames.map((filename, idx) => {
